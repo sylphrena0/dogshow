628a2ba16cd5efad3d3d6165a76a49d1c15416b6
Sylphrena <ksylphrena@gmail.com>
2023-02-21 14:45:14 -0500

    started on layout for login and formatted fields

--- src/GUI/HomePage.java
+++ src/GUI/HomePage.java
@@ -4,16 +4,73 @@ import config.ConfigParameters;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class HomePage extends JPanel implements ConfigParameters {
     private Controller controller;
     private JPanel homePanel, authPanel, loginPanel, registrationPanel, dogImagePanel;
     private JPasswordField passwordField;
     private JTextField usernameField;
+    private Color inputColor = new Color(0x434343);
+    private Dimension authPanelSize = new Dimension((int) (0.7*screenSize.width), (int) (0.5*screenSize.height));
 
     public HomePage() {
         this.controller = Controller.getInstance("Home - Dog Show");
         addComponents();
+    }
+
+    private void configureInputField(JTextField textField, String placeholder) {
+        textField.addActionListener(controller);
+        textField.setForeground(Color.WHITE);
+        textField.setCaretColor(Color.WHITE);
+        textField.setBorder(BorderFactory.createEmptyBorder(10,20,10,20));
+        textField.setSize(new Dimension((int) (0.5*authPanelSize.width), (int) (0.25*authPanelSize.height)));
+        textField.setFont(new Font("Arial", 1, 24));
+        textField.setText(placeholder);
+
+        textField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (textField.getText().equals(placeholder)) {
+                    textField.setText("");
+                }
+            }
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (textField.getText().isEmpty()) {
+                    textField.setText(placeholder);
+                }
+            }
+        });
+    }
+
+    private void configurePasswordField(JPasswordField passwordField, String placeholder) {
+        passwordField.addActionListener(controller);
+        passwordField.setForeground(Color.WHITE);
+        passwordField.setCaretColor(Color.WHITE);
+        passwordField.setBorder(BorderFactory.createEmptyBorder(10,20,10,20));
+        passwordField.setSize(new Dimension((int) (0.5*authPanelSize.width), (int) (0.25*authPanelSize.height)));
+        passwordField.setFont(new Font("Arial", 1, 24));
+        passwordField.setEchoChar((char) 0); //sets password to visible
+        passwordField.setText(placeholder);
+
+        passwordField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (new String(passwordField.getPassword()).equals(placeholder)) {
+                    passwordField.setText("");
+                    passwordField.setEchoChar('*'); //sets password to invisible
+                }
+            }
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (new String(passwordField.getPassword()).isEmpty()) {
+                    passwordField.setText(placeholder);
+                    passwordField.setEchoChar((char) 0); //sets password to visible
+                }
+            }
+        });
     }
 
     private void addComponents() {
@@ -25,68 +82,66 @@ public class HomePage extends JPanel implements ConfigParameters {
 
         CardLayout authLayout = new CardLayout();
         authPanel = new JPanel(authLayout);
-        authPanel.setPreferredSize(new Dimension((int) (0.7*screenSize.width), (int) (0.5*screenSize.height)));
+        authPanel.setPreferredSize(authPanelSize);
 
         authPanel.setBorder(BorderFactory.createEmptyBorder(80, 80, 80, 600));
         authPanel.setBackground(backgroundColor);
 
-//        authPanel.setUndecorated(true);
-//        authPanel.setShape(new RoundRectangle2D.Double(x, y, 100, 100, 50, 50));
-
-
         loginPanel = new RoundedPanel();
-        registrationPanel = new JPanel();
+        registrationPanel = new RoundedPanel();
 
         loginPanel.setBackground(headerColor);
         registrationPanel.setBackground(headerColor);
 
+        JLabel welcomeText = new JLabel("Welcome! Please Login:");
+        welcomeText.setFont(new Font("Caveat",1, 75));
+        welcomeText.setForeground(Color.WHITE);
+        welcomeText.setHorizontalAlignment(SwingConstants.LEFT);
 
-        JLabel usernameLabel = new JLabel("Enter your login: ", JLabel.CENTER);
-        usernameLabel.setLabelFor(usernameField);
-
-        JLabel passwordLabel = new JLabel("Enter the password: ", JLabel.CENTER);
-        passwordLabel.setLabelFor(passwordField);
 
         JButton loginButton = new JButton("Login");
         loginButton.addActionListener(controller);
+        loginButton.setBackground(new Color(0x93c47d));
+        loginButton.setForeground(Color.BLACK);
 
-        JButton createAccount = new JButton("Create");
-        createAccount.addActionListener(controller);
+        JButton registerButton = new JButton("Register");
+        registerButton.addActionListener(controller);
+        registerButton.setBackground(new Color(0x7c50e2));
+        registerButton.setForeground(Color.WHITE);
 
-        usernameField = new JTextField("jdoe@somesuch",10);
+        usernameField = new RoundedTextField(inputColor);
         usernameField.setActionCommand("LOGIN");
-        usernameField.addActionListener(controller);
+        configureInputField(usernameField, "Username");
 
-        passwordField = new JPasswordField(10);
+        passwordField = new RoundedPasswordField(inputColor);
         passwordField.setActionCommand("PASSWORD");
-        passwordField.addActionListener(controller);
+        configurePasswordField(passwordField, "Password");
 
         GroupLayout loginLayout = new GroupLayout(loginPanel);
-        loginLayout.setAutoCreateGaps(false);
-        loginLayout.setAutoCreateContainerGaps(false);
+        loginLayout.setAutoCreateGaps(true);
+        loginLayout.setAutoCreateContainerGaps(true);
 
         loginLayout.setHorizontalGroup(
-                loginLayout.createSequentialGroup()
-                        .addGroup(loginLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                .addComponent(usernameLabel)
-                                .addComponent(passwordLabel)
-                                .addComponent(loginButton))
-                        .addGroup(loginLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
-                                .addComponent(usernameField)
-                                .addComponent(passwordField)
-                                .addComponent(createAccount))
+                loginLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+                        .addGroup(GroupLayout.Alignment.LEADING, loginLayout.createSequentialGroup()
+                                .addGap(60)
+                                .addComponent(welcomeText)
+                        )
+                        .addComponent(usernameField, 400, 500, 600)
+                        .addComponent(passwordField, 400, 500, 600)
+                        .addGroup(loginLayout.createSequentialGroup()
+                                .addComponent(loginButton)
+                                .addComponent(registerButton))
         );
         loginLayout.setVerticalGroup(
                 loginLayout.createSequentialGroup()
-                        .addGroup(loginLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(usernameLabel)
-                                .addComponent(usernameField))
-                        .addGroup(loginLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                .addComponent(passwordLabel)
-                                .addComponent(passwordField))
-                        .addGroup(loginLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                        .addGap(30)
+                        .addComponent(welcomeText)
+                        .addComponent(usernameField, 30, 50, 70)
+                        .addComponent(passwordField, 30, 50, 70)
+                        .addGroup(loginLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
                                 .addComponent(loginButton)
-                                .addComponent(createAccount))
+                                .addComponent(registerButton))
         );
 
         loginPanel.setLayout(loginLayout);
@@ -99,6 +154,4 @@ public class HomePage extends JPanel implements ConfigParameters {
 
         this.add(authPanel, BorderLayout.NORTH);
     }
-
-
 }
---
+++ src/GUI/RoundedPasswordField.java
@@ -0,0 +1,34 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+// Author: Graywolf
+// Source: https://stackoverflow.com/questions/16213836/java-swing-jtextfield-set-placeholder
+
+public class RoundedPasswordField extends JPasswordField {
+    private Shape shape;
+    private Color color;
+    public RoundedPasswordField(Color color) {
+//        super(10);
+        this.color = color;
+        setOpaque(false); // As suggested by @AVD in comment.
+
+    }
+    protected void paintComponent(Graphics g) {
+        g.setColor(color);
+        g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+        super.paintComponent(g);
+    }
+    protected void paintBorder(Graphics g) {
+        g.setColor(color);
+        g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+    }
+    public boolean contains(int x, int y) {
+        if (shape == null || !shape.getBounds().equals(getBounds())) {
+            shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+        }
+        return shape.contains(x, y);
+    }
+}
