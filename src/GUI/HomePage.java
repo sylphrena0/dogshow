e32abeba3504e8532b7c01c626aa4273818cef6c
Sylphrena <ksylphrena@gmail.com>
2023-02-22 17:22:26 -0500

    formatted

--- src/GUI/HomePage.java
+++ src/GUI/HomePage.java
@@ -80,11 +80,10 @@ public class HomePage extends JPanel implements ConfigParameters {
         button.setBorderPainted(false);
     }
     private void addComponents() {
-        //"this" is the JPanel we are adding to the super, as this class extend JPanel
+        //"this" is the JPanel we are adding to the super, as this class extends JPanel
         this.setBackground(backgroundColor);
         this.setPreferredSize(pageSize);
         this.setLayout(new BorderLayout());
-
 
         authLayout = new CardLayout();
         authPanel = new JPanel(authLayout);
