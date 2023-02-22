1d6b199906b7f5c6baf37af482bf43ffbf3e178d
Sylphrena <ksylphrena@gmail.com>
2023-02-22 17:15:51 -0500

    Added inputColor and removed unneeded declarations

--- src/config/ConfigParameters.java
+++ src/config/ConfigParameters.java
@@ -3,9 +3,9 @@ package config;
 import java.awt.*;
 
 public interface ConfigParameters {
-    public static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    public static  Dimension pageSize = new Dimension(screenSize.width, screenSize.height - 120);
-    public static Color backgroundColor = new Color(0x212121);
-    public static Color headerColor = new Color(0x303030);
-
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    Dimension pageSize = new Dimension(screenSize.width, screenSize.height - 120);
+    Color backgroundColor = new Color(0x212121);
+    Color headerColor = new Color(0x303030);
+    Color inputColor = new Color(0x434343);
 }
