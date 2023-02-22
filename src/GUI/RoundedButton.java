0f1fdc89402cb3d6dd92231793527fa3b7afb8b7
Sylphrena <ksylphrena@gmail.com>
2023-02-22 17:12:08 -0500

    added RoundedButton class

---
+++ src/GUI/RoundedButton.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class RoundedButton extends JButton {
+    private Shape shape;
+    private Color color;
+    public RoundedButton(Color color) {
+        this.color = color;
+        setOpaque(false); // As suggested by @AVD in comment.
+
+    }
+    protected void paintComponent(Graphics g) {
+        g.setColor(color);
+        g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+        super.paintComponent(g);
+    }
+    protected void paintBorder(Graphics g) {
+        g.setColor(color);
+        g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+    }
+    public boolean contains(int x, int y) {
+        if (shape == null || !shape.getBounds().equals(getBounds())) {
+            shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+        }
+        return shape.contains(x, y);
+    }
+}
