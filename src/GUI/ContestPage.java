498ba0ef644f94d598e1f41531b411ba46fdb5eb
Sylphrena <ksylphrena@gmail.com>
2023-02-22 17:22:03 -0500

    added other page stubs

---
+++ src/GUI/ContestPage.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import config.ConfigParameters;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ContestPage extends JPanel implements ConfigParameters {
+    private Controller controller;
+
+    public ContestPage() {
+        this.controller = Controller.getInstance("Registration - Dog Show");
+        addComponents();
+    }
+
+    private void addComponents() {
+        //"this" is the JPanel we are adding to the super, as this class extends JPanel
+        this.setBackground(backgroundColor);
+        this.setPreferredSize(pageSize);
+        this.setLayout(new BorderLayout());
+
+    }
+}
---
+++ src/GUI/RecordsPage.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import config.ConfigParameters;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RecordsPage extends JPanel implements ConfigParameters {
+    private Controller controller;
+
+    public RecordsPage() {
+        this.controller = Controller.getInstance("Records - Dog Show");
+        addComponents();
+    }
+
+    private void addComponents() {
+        //"this" is the JPanel we are adding to the super, as this class extends JPanel
+        this.setBackground(backgroundColor);
+        this.setPreferredSize(pageSize);
+        this.setLayout(new BorderLayout());
+
+    }
+}
