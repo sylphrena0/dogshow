7d4de8ee7c09615c31acbfbf28b4f90d84c0d6c0
Sylphrena <ksylphrena@gmail.com>
2023-05-04 19:58:37 -0400

    removed non-working rounded code

--- src/GUI/components/RoundedDropdown.java
+++ src/GUI/components/RoundedDropdown.java
@@ -8,15 +8,13 @@ import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
 
 public class RoundedDropdown extends JComboBox<String> implements Parameters {
-    private Shape shape;
     private final String text;
-    private final Color color;
-
     public RoundedDropdown(String text, Controller controller) {
+        //TODO: make this actually rounded
         super(new String[] {});
-        this.color = inputColor;
-        this.text = text;
+//        this.color = inputColor;
 //        this.setSelectedIndex(0);
+        this.text = text;
         this.addActionListener(controller);
         this.setForeground(Color.WHITE);
         this.setBackground(inputColor);
@@ -24,30 +22,11 @@ public class RoundedDropdown extends JComboBox<String> implements Parameters {
         this.setBorder(componentInsets);
     }
 
-    protected void paintComponent(Graphics g) {
-        g.setColor(color);
-        g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
-        super.paintComponent(g);
-    }
-    protected void paintBorder(Graphics g) {
-        g.setColor(color);
-        g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
-    }
-    public boolean contains(int x, int y) {
-        if (shape == null || !shape.getBounds().equals(getBounds())) {
-            shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 15, 15);
-        }
-        return shape.contains(x, y);
-    }
-
     @Override
-    public Object getSelectedItem()
-    {
+    public Object getSelectedItem() {
         Object selected = super.getSelectedItem();
-
         if (selected == null)
             selected = text;
-
         return selected;
     }
 
